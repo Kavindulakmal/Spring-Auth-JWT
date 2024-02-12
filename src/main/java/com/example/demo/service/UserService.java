d73fa7e2c9207feafd9df7d752331247108a63a4
Kavindulakmal <avakavindu@gmail.com>
2024-02-12 18:37:39 +0530

    First Commit

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,57 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.RoleDao;
+import com.example.demo.dao.UserDao;
+import com.example.demo.entity.Role;
+import com.example.demo.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private RoleDao roleDao;
+
+    public User registerNewUser(User user){
+        return  userDao.save(user);
+    }
+
+    public void initRolesAndUser(){
+        Role adminRole = new Role();
+        adminRole.setRoleName("Admin");
+        adminRole.setRoleDescription("Admin Role");
+        roleDao.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setRoleName("User");
+        userRole.setRoleDescription("Default role for new created record");
+        roleDao.save(userRole);
+
+        User adminUser = new User();
+        adminUser.setUserFirstName("admin");
+        adminUser.setUserLastName("admin");
+        adminUser.setUserName("admin123");
+        adminUser.setUserPassword("admin@pass");
+        Set<Role> adminRoles = new HashSet<>();
+        adminUser.setRole(adminRoles);
+        adminRoles.add(adminRole);
+        userDao.save(adminUser);
+
+        User user = new User();
+        user.setUserFirstName("kavindu");
+        user.setUserLastName("lakmal");
+        user.setUserName("kavindu123");
+        user.setUserPassword("kavndu@pass");
+        Set<Role> userRoles = new HashSet<>();
+        userRoles.add(userRole);
+        user.setRole(userRoles);
+        userDao.save(user);
+    }
+}
